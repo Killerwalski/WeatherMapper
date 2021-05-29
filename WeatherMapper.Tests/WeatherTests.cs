03d0e2a4d468c0b43a1b4d31ba132b8b778da25a
Killerwalski <pakowalski86@gmail.com>
2021-05-29 11:40:32 -0400

    Add Test proj

---
+++ WeatherMapper.Tests/WeatherTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WeatherMapper.Tests
+{
+    public class WeatherTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WeatherMapper.Web/Data/OpenWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMapper.Web.Data
+{
+    public class OpenWeatherService : IWeatherService
+    {
+    }
+}
